50879da5b08066e5b427b363b11272531ff08104
shraddhabante27 <shraddhabante27@gmail.com>
2022-12-08 13:45:53 +0530

    changes js file

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-// implicit conversion
+// implicit coercion:
 console.log('A' - 1);//Nan
 console.log('A' + 1);//A1
 console.log(2 + '2' + '2');//222
@@ -10,24 +10,33 @@ console.log(2 - '2' + 90);//90
 console.log(89 - '90' / 2);//44
 console.log(2 + '2' + null);//22null
 console.log((true == false) > 2);//false
-console.log((89 + 'hello' + 90 / 9));//89hello10
+console.log(89 + 'hello' + 90 / 9);//89hello10
 
 // 3.	Datatypes:
-var str = ' gfuh ieiuei ';
-console.log(str.slice(1, 6)); //extract first five letters from a string (' gfuh ieiuei ')
 
+//extract first five letters from a string (' gfuh ieiuei ')
+var str = ' gfuh ieiuei ';
+console.log(str.slice(1, 6));
+
+// get the length of a string and make it uppercase ('hduej dij')
 var strTwo = 'hduej dij';
 console.log(strTwo.length);
-console.log(strTwo.toUpperCase());// get the length of a string and make it uppercase ('hduej dij')
+console.log(strTwo.toUpperCase());
 
+//take a string, make it lowercase and trim it ('   biji jdo   ')
 var strThree = '   biji jdo   ';
-console.log(strThree.toLowerCase()); //take a string, make it lowercase and trim it ('   biji jdo   ')
+console.log(strThree.toLowerCase());
 console.log(strThree.trim());
 
+//replace specified word in a string ('', '')
 var _str4 = ' html is used create structure of the website';
-console.log(_str4.replace('html', 'HTML')); //replace specified word in a string ('', '')
+console.log(_str4.replace('html', 'HTML'));
 
-console.log((89 + 'hello' + 90 / 9)); //89hello10
+// random statements in implicit coercion e.g. (89  + 'hello' + 90 / 9)
+console.log('hello' + 20 * 10);//hello200
+console.log(20 + '20' + 'hello');
+console.log(20 + 'hiii' + 40 % 5);
+console.log('world' + 20 / 10 * 3);
 
 // 	create an object for car.
 var car = {
